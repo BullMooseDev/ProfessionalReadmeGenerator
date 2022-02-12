63d48e827b6f94913caed171c7bc4881aa83ab16
thatblondetitan <koleervine@gmail.com>
2022-02-12 16:29:33 -0700

    post tutor changes

---
+++ dist/readme.md
@@ -0,0 +1,5 @@
+# 123
+            this is a test line that I want to see in my new file
+
+           readmegif =  img![source code]
+            
--- src/readme-template.js
+++ src/readme-template.js
@@ -1,17 +1,28 @@
 const generateReadme = require('../dist/utils/generate-readme');
 
 const reamdeTemplate = questionData => {
-            `# ${projectname}
-            ## ${description}
-            ## ${tableofcontents}
-            ## ${installation}
-            ## ${usage}
-            ## ${credits}
-            ## ${license}
-            ## ${badges}
-            ## ${features}
-            ## ${howToContribute}
-            ## ${tests}`;
+    let licenseSection = "No license selected."
+
+    if(questionData.license == "MIT") {
+        licenseSection = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+    }
+    return `# ${questionData.readmeTitle}
+this is a test line that I want to see in my new file
+
+## License
+
+${licenseSection}
+`;
+    // ## ${description}
+    // ## ${tableofcontents}
+    // ## ${installation}
+    // ## ${usage}
+    // ## ${credits}
+    // ## ${license}
+    // ## ${badges}
+    // ## ${features}
+    // ## ${howToContribute}
+    // ## ${tests}
 };
 
-module.exports('../dist/utils/generate-readme.js');
+module.exports = reamdeTemplate;
